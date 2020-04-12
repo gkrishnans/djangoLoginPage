c843a1242c9f97c9a50d3fd5d6a4cb0733d06d51
gkrishnans <gokul.hema08@gmail.com>
2020-04-12 17:45:06 +0530

    final

--- loginpage/loginpage/settings.py
+++ loginpage/loginpage/settings.py
@@ -122,5 +122,10 @@ DATABASE['default'].update(db_from_env)
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
-
+PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR,"static"),
+]
+STATIC_ROOT = os.path.join(PROJECT_ROOT,"staticfiles")
+STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
